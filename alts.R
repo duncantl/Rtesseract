6437871a465651297121032b88011030ae1b0bb4
Duncan Temple Lang <duncan@r-project.org>
2015-05-06 17:44:37 -0700

    moved these examples to Rd files

--- alts.R
+++
@@ -1,6 +0,0 @@
-library(Rtesseract)
-f = system.file("images", "IMG_1234.jpg", package = "Rtesseract")
-ans = .Call("R_ocr_alternatives", f, c(save_blob_choices = "T", "tessedit_char_whitelist" = "0123456789."), 3L)
-
-
-# , "save_best_choices" = "T",
--- conf.R
+++
@@ -1,4 +0,0 @@
-library(Rtesseract)
-f = system.file("images", "IMG_1234.jpg", package = "Rtesseract")
-# seg faults if set save_blob_choices = "T"
-o = ocr(f, opts = c("tessedit_char_whitelist" = "0123456789.", "save_blob_choices" = "F"), "symbol")
