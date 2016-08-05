1bfc4e61aea2d1586cd2884b9fc737629fe84919
Duncan Temple Lang <duncan@r-project.org>
2016-08-04 21:06:05 -0700

    reading a box file and plotting it independently of plot.OCR

---
+++ R/readBox.R
@@ -0,0 +1,73 @@
+if(FALSE) {
+    library(Rtesseract)
+#    library(png)
+#    img = readPNG(system.file("trainingSample", "eng.tables.ex0.png", package = "Rtesseract"))
+    d = readBoxFile(system.file("trainingSample", "eng.tables.ex0.box", package = "Rtesseract"), dim(img))
+    plot(d)
+
+    plot(d[ d[,2] > 1500, ], cex = 1.2)
+#    Rtesseract:::plot.OCR(, img = NULL, bbox = d, filename = "foo", showText = TRUE, dim = dim(img))
+}
+
+readBoxFile =
+    # Read a file  created as part of the training.
+    # e.g., tesseract eng.tables.ex0.png eng.tables.ex0 batch.nochop makebox
+    # There is an example in inst/trainingSample/eng.tables.ex0.box
+    #
+    # This returns a data frame with the columns reordered to be like a bounding box
+    # This is not like a bounding box exactly as that is a matrix and the rownames are the
+    # values for the corresponding box.  But they are not necessarily unique and so
+    # cannot be rownames of a data frame.
+    #
+    # This adds the class TesseractBoxdata to the data frame so that we can dispatch methods on this.
+    # e.g. getTextInfo()
+    #
+function(file, imgDim = integer(), ...)
+{
+   d = read.table(file, stringsAsFactors = FALSE, quote = '', comment.char = '', ...)
+   d = d[, c(2, 3, 4, 5, 1)]
+   colnames(d) = c("left", "bottom", "right", "top", "text")
+   class(d) = c("TesseractBoxdata", class(d))
+
+#  if(length(imgDim) == 0)
+#     r =  max(unlist(d[, c(2, 4)]))
+#  else
+#     r = imgDim[1]
+#
+#  d[,2] = r- d[,2]
+#  d[,4] = r- d[,4]    
+   
+   if(is.character(file))
+      attr(d, "file") = file
+   
+   d
+}
+
+getTextInfo =
+function(x, ...)
+  UseMethod("getTextInfo")
+
+getTextInfo.default =
+function(x, ...)
+    NULL
+
+getTextInfo.TesseractBoxdata =
+function(x, ymax = NA, ...)
+{
+  ans = x[, c("left", "bottom", "text")]
+# if(is.na(ymax))
+#     ymax = max(ans[,2])
+# ans[,2] =  ymax - ans[,2]
+  ans
+}
+    
+plot.TesseractBoxdata =
+function(x, cex = .9, ...)
+{
+    plot(0, type = "n", xlim = range(x[, c(1, 3)]), ylim = range(x[, c(2, 4)]), xlab = "", ylab = "")
+    rect(d[,1], d[,2], d[,3], d[, 4])
+    opar = par(no.readonly = TRUE)
+    on.exit(par(opar))
+    par(pty = "s")
+    text(d[,1], d[,2], d[, "text"], adj = c(-0.2, -.2), cex = cex)
+}
