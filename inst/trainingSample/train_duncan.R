94c3efd8f5bb7cafe5464f22b8f0d4de125fb387
Duncan Temple Lang <duncan@r-project.org>
2016-08-05 07:29:46 -0700

    my incomplete changes to use some of the functions I added to the package to read and plot the box file

--- inst/trainingSample/train_duncan.R
+++ inst/trainingSample/train_duncan.R
@@ -2,28 +2,28 @@
 ## M. Espe
 ## Aug 2016
 
-setwd("~/DSI/PlantTables/training/")
+# setwd("~/DSI/PlantTables/training/")
 
 system("tesseract eng.tables.exp0.png eng.tables.exp0 batch.nochop makebox")
 
+
 doc <- readLines("1990_p44.txt")
-boxlines <- readLines("eng.tables.exp0.box")
-boxlines <- strsplit(boxlines, " ")
-boxlines <- do.call(rbind, boxlines)
-
-bb <- apply(boxlines[,2:5], 2, as.numeric)
-
-plot(0, type = "n",
-     xlim = c(min(bb[,1]), max(bb[,3])),
-     ylim = c(min(bb[,2]), max(bb[,4])))
-rect(bb[,1], bb[,2], bb[,3], bb[,4])
 doc <- gsub(" ", "", doc)
 chars <- unlist(strsplit(doc, ""))
 
 
+library(Rtesseract)
+bb = readBoxFile("eng.tables.exp0.box")
+plot(bb)
+
+
 ## Got to remove those boxes that span the width of the table
+rr = bb[ bb[, "text"] == "~", ]
+rect(rr[,1], rr[, 2], rr[, 3], rr[, 4], col = "red")
+
 rect(bb[53:54,1], bb[53:54,2], bb[53:54,3], bb[53:54,4], col = "red")
 rect(bb[695,1], bb[695,2], bb[695,3], bb[695,4], col = "red")
+
 
 lens <- bb[,3] - bb[,1]
 lens[lens>1000]
@@ -52,5 +52,7 @@ writeLines(c("v2\nW MM 0",
              "0 O 0",
              "O 0 0"), con = "unicharambigs")
 list.files()
+
+# I'd do a file.rename() here
 system('for f in shapetable normproto inttemp pffmtable unicharset unicharambigs; do mv "$f" "eng.$f"; done')
 system("combine_tessdata eng.")
