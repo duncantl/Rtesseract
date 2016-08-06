037ea76e6a854652d903aacab9a30d6b76901c69
Duncan Temple Lang <duncan@r-project.org>
2016-08-06 11:46:50 -0700

    all the enums

--- TU/tu.R
+++ TU/tu.R
@@ -7,5 +7,15 @@ e = getEnums(tu)
 k = getCppClasses(tu)
 
 
-cat(makeEnumClass(e$PageSegMode), file = "../R/PageSegMode.R", sep = "\n")
-cat('#include "Rtesseract.h"', '#include "RConverters.h"', "", makeEnumDef(e$PageSegMode, namespace = "tesseract"), sep = "\n", file = "../src/PageSegMode.cc")
+#cat(makeEnumClass(e$PageSegMode), file = "../R/PageSegMode.R", sep = "\n")
+cat(unlist(lapply(e, makeEnumClass)), file = "../R/PageSegMode.R", sep = "\n")
+
+# makeEnumDef(e$PageSegMode, namespace = "tesseract"),
+ccode = lapply(e, function(e) {
+                    p = getCursorSemanticParent(e@def)
+                    makeEnumDef(e, namespace = if(p$kind == CXCursor_Namespace) "tesseract" else character())
+                  })
+cat('#include "Rtesseract.h"',
+    '#include "RConverters.h"', "",
+    unlist(ccode),
+   sep = "\n", file = "../src/PageSegMode.cc")
