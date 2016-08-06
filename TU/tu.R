e4908a85b4f2aeeae3ca82e9223588389639dbbe
Duncan Temple Lang <duncan@r-project.org>
2016-08-06 10:54:37 -0700

    code to generate PageSegMode enums

--- TU/tu.R
+++ TU/tu.R
@@ -1,6 +1,11 @@
 library(RCIndex)
+library(RCodeGen)
 tu = createTU("tess.cc", include = "/usr/local/include/tesseract")
 
 e = getEnums(tu)
 
 k = getCppClasses(tu)
+
+
+cat(makeEnumClass(e$PageSegMode), file = "../R/PageSegMode.R", sep = "\n")
+cat('#include "Rtesseract.h"', '#include "RConverters.h"', "", makeEnumDef(e$PageSegMode, namespace = "tesseract"), sep = "\n", file = "../src/PageSegMode.cc")
