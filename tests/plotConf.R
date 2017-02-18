dee33780133748553bfa713aff616495c19bc6dc
Duncan Temple Lang <duncan@r-project.org>
2017-02-17 18:37:40 -0800

    show how to display confidence of matches via colors

--- tests/plotConf.R
+++ tests/plotConf.R
@@ -3,20 +3,25 @@ library(Rtesseract)
 f = system.file("trainingSample", "eng.tables.exp0.png", package = "Rtesseract")
 
 ts = tesseract(f)
-Recognize(ts)
+#Recognize(ts)
 bbox = getBoxes(ts)
 
 library(png)
-colors = Rtesseract:::getConfidenceColors(bbox)
+colors = getConfidenceColors(bbox)  # Rtesseract:::
 plot(ts, bbox = bbox, border = colors)
 
 
+dev.new()
 m = plot(ts, img = NULL, bbox = bbox, border = colors)
 rect(m[,1], m[,2], m[,3], m[,4], col = colors)
 
-
+dev.new()
 m = plot(ts, img = NULL, bbox = bbox, border = colors, cropToBoxes = TRUE)
 rect(m[,1], m[,2], m[,3], m[,4], col = colors)
 
 
+
+# default now
+dev.new()
+plot(ts, bbox = bbox, border = colors)
 
