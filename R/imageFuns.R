cca5e64b95679632453ee6407bb66dfe05a03c50
Matt Espe <mespe@ucdavis.edu>
2017-03-02 15:16:12 -0800

    Added functionality to plot more than just pngs

--- R/imageFuns.R
+++ R/imageFuns.R
@@ -1,5 +1,10 @@
 readImage =
 function(filename)
 {
-    png::readPNG(filename)
+    if(grepl("\\.png$", filename))
+        png::readPNG(filename)
+    if(grepl("\\.je?pg$", filename))
+        jpeg::readJPEG(filename)
+    if(grepl("\\.tiff?$", filename))
+        rtiff::readTiff(filename)
 }
