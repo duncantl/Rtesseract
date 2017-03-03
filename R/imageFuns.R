ed295af663a00c6ff7cd08fc1ddb2976b3603ab0
Matt Espe <mespe@ucdavis.edu>
2017-03-02 17:28:02 -0800

    Added code to be able to plot all image formats

--- R/imageFuns.R
+++ R/imageFuns.R
@@ -2,9 +2,11 @@ readImage =
 function(filename)
 {
     if(grepl("\\.png$", filename))
-        png::readPNG(filename)
+        img = png::readPNG(filename)
     if(grepl("\\.je?pg$", filename))
-        jpeg::readJPEG(filename)
+        img = jpeg::readJPEG(filename)
     if(grepl("\\.tiff?$", filename))
-        rtiff::readTiff(filename)
+        img = tiff::readTIFF(filename)
+    img
 }
+
---
+++ tests/plotAllFormats.R
@@ -0,0 +1,17 @@
+# PNG
+a <- system.file("images", "OCRSample2.png", package = "Rtesseract")
+
+api1 <- tesseract(a)
+plot(api1)
+
+#TIFF
+b <- system.file("images", "OCRSample2.tiff", package = "Rtesseract")
+
+api2 <- tesseract(b)
+plot(api2)
+
+# JPEG
+c <- system.file("images", "OCRSample2.jpg", package = "Rtesseract")
+
+api3 <- tesseract(c)
+plot(api3)
