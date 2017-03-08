6a51df8ccd58a71a19b1dd5eb570ec2bfbd30a76
Matt Espe <mespe@ucdavis.edu>
2017-03-08 14:46:51 -0800

    Different function to find the column edges

--- R/tables.R
+++ R/tables.R
@@ -1,4 +1,4 @@
-
+if(FALSE){
 findColumns =
 function(api, bbox = GetBoxes(api, level = level), level = 3)
 {
@@ -6,4 +6,14 @@ function(api, bbox = GetBoxes(api, level = level), level = 3)
     qleft = quantile(bbox[, "left"], seq(0, 1, by = .1))
     qright = quantile(bbox[, "right"], seq(0, 1, by = .1))    
     return(data.frame(qleft = qleft, qright = qright))
+}
+}
+
+
+findColumns =
+function(api, bbox = GetBoxes(api, level = level), level = 3, ncols, side = "left")
+{
+    dens <- density(bbox[,side], adjust = 0.001)
+    i <- order(dens$y, decreasing=TRUE)
+    dens$x[i][1:ncols]
 }
