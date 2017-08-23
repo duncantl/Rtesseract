5655a5d8d3d688d06330fcae915f97fc07ad37c7
Duncan Temple Lang <duncan@r-project.org>
2017-08-23 12:25:43 -0700

    leptonica version and supported image  libraries

--- R/version.R
+++ R/version.R
@@ -1,3 +1,29 @@
 tesseractVersion =
 function()
    .Call("R_tesseract_Version")
+
+
+leptonicaVersion =
+function()
+{
+    .Call("R_getLeptonicaVersion")
+}
+
+getImageLibs =
+function(asMatrix = FALSE)
+{
+    x = .Call("R_getImagelibVersions")
+    x = trim(strsplit(x, ":")[[1]])
+    nms = gsub("^([a-zA-Z]+) .*", "\\1", x)
+    nums = gsub("^([a-zA-Z]+) ", "", x)
+    names(nums) = nms
+    if(asMatrix) {
+        matrix(as.integer(unlist( strsplit(nums, "\\."))), , 3, byrow = TRUE, dimnames = list(nms, c("major", "minor", "patch")))
+    } else 
+       nums
+}
+
+
+trim =
+function (x) 
+   gsub("(^[[:space:]]+|[[:space:]]+$)", "", x)    
