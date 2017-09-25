d2eb4a87115c1d8615b0d2db434caee03faa7c51
Duncan Temple Lang <duncan@r-project.org>
2017-09-25 09:12:35 -0700

    using REvprintf() for now so that we can pass ... onto that w/o having to look at/interpret the format.  Should be generating a warning. Could use a text connection and write as there is an R routine which also supports a va_list.   Note using < rather than [ since the [ is akward to grep for in configure.ac, i.e. m4 escapes

--- src/Rexit/exit.c
+++ src/Rexit/exit.c
@@ -5,16 +5,26 @@
 void
 exit(int status)
 {
-  fprintf(stderr, "In local exit (Rexit)\n");
-  PROBLEM "exiting from tesseract! Caught locally"
+//  fprintf(stderr, "In local exit (Rexit)\n");
+  PROBLEM "[tesseract 'exit']"
       ERROR;
 }
 
+#include <stdio.h>
+#include <stdarg.h>
+
+#define R_USE_C99_IN_CXX 1
+#include <R_ext/Print.h>
 //extern "C"
 void tprintf_internal(const char *format, ...)
 {
-  fprintf(stderr, "In local tprintf (Rexit)\n");    
-    PROBLEM "[local] tprintf_internal"
-      WARN;
+//  fprintf(stderr, "In local tprintf (Rexit)\n");    
+//    char buf[100];
+//    sprintf(buf, "[tesseract] %s", format);
+    va_list args;
+    REprintf("%s", "<tesseract> ");
+    va_start(args, format);
+    REvprintf(format, args);
+    va_end(args);
 }
 
