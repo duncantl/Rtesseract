3af9c399c9e8837faafd788f37e9931bf4234468
Duncan Temple Lang <duncan@r-project.org>
2017-10-04 10:00:25 -0700

    changed to findLines and adding a getLines() function to get the actual x0,y0, x1,y1 info. findLines() now more flexible with the constants settable via parameters

--- R/leptonicaLines.R
+++ R/leptonicaLines.R
@@ -1,23 +1,44 @@
 getLines =
-    # Could reuse all the pixmaps
-function(pix, hor, vert, asLines = TRUE, invert = !asLines)
+function(pix, lineThreshold = .1, hor, vert, ..., asIs = is(pix, "AsIs"))
 {
-   p3 = pixCloseGray(pix, hor, vert)
-   p4 = pixErodeGray(p3, 3, 5)
+  if(!asIs)
+     pix = findLines(pix, hor, vert, ...)
 
-   p5 = pixThresholdToValue(p4, 210, 255)
-   p6 = pixThresholdToValue(p5, 210, 0)
+  img = pixGetPixels(pix)
+  horizontal = hor > vert
+  r = if(horizontal) 
+         rowSums(img)
+      else
+         colSums(img)
+
+  if(lineThreshold < 1)
+      lineThreshold = lineThreshold * if(horizontal) nrow(img) else ncol(img)
+
+  w = r > lineThreshold
+  
+}
+
+findLines =
+    # Could reuse all the pixmaps
+function(pix, hor, vert, asLines = TRUE, invert = !asLines, erode = c(3, 5), threshold = 210)
+{
+    p3 = pixCloseGray(pix, hor, vert)
+    if(length(erode))
+       p3 = pixErodeGray(p3, erode[1], erode[2])
+
+   p5 = pixThresholdToValue(p3, threshold, 255)
+   p6 = pixThresholdToValue(p5, threshold, 0)
 
    # We don't use this again in these computations, but we want it to get the actual lines.
    if(asLines) {
-       tmp = pixThresholdToBinary(p6, 210)
+       tmp = pixThresholdToBinary(p6, threshold)
        if(invert)
           tmp = pixInvert(tmp)
        return(tmp)
    }
    
    if(invert)
-       pixInvert(p6)
+      pixInvert(p6)
    else
-       p6
+      p6
 }
