293ec2a07f522e38324964120ff5d4c6a3b39051
Duncan Temple Lang <duncan@r-project.org>
2017-10-04 09:57:28 -0700

    plot.Pix method allows caller to specify color or not and the maximum pixel value to scale by

--- R/plotPix.R
+++ R/plotPix.R
@@ -1,14 +1,23 @@
 plot.Pix =
-function(x, y , rgb = FALSE, ...)   
+    #
+    # p = pixRead("inst/images/DifferentFonts.png")
+    # p2 = pixConvertTo8(p)
+    #
+function(x, y, rgb = pixGetDepth(x) > 8, pixMax = max(pix), ...)   
 {
     pix = if(rgb)
              pixGetRGBPixels(x)
           else
              pixGetPixels(x) # 
 
-    pix[] = pix/255
-    # Have to normalize
-#    browser()
-    plot(0, xlim = c(1, ncol(pix)), ylim = c(1, nrow(pix)), ...)
+    pix[] = pix/pixMax
+
+#    opar = par(no.readonly = TRUE)
+#    on.exit(par(opar))
+#    par(mar = c(2.1, 2.1, 4.1, 2.1))
+    plot(0, type = "n", xlim = c(1, ncol(pix)), ylim = c(1, nrow(pix)), xlab = "", ylab = "", ...)
     rasterImage(pix, 1, 1, ncol(pix), nrow(pix))
 }
+
+setMethod("plot", "Pix", plot.Pix)
+
