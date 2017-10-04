6e2d9ba4f7c9e529ad3c80638e455be0a2370318
Duncan Temple Lang <duncan@r-project.org>
2017-10-04 11:13:05 -0700

    testing the plot of a tesseract object with a Pix as the img argument

--- tests/tessPlotPix.R
+++ tests/tessPlotPix.R
@@ -1,9 +1,14 @@
 library(Rtesseract)
-
 f = system.file("images", "DifferentFonts.png", package = "Rtesseract")
+f = system.file("images", "SMITHBURN_1952_p3.png", package = "Rtesseract")
 pix = pixRead(f)
 tess = tesseract(pix)
 plot(tess, img = pix)
+
+# Now do it without the pix
+# Could set the name on the tesseract object. Will that cause it to re-recognize?
+#SetInputName(tess, f)
+plot(tess, filename = f)
 
 
 
