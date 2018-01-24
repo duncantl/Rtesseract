a9869d2ee35fb7c30e538bb160fba3532ece2ab8
Matt Espe <mespe@ucdavis.edu>
2018-01-23 16:11:08 -0800

    Add option to invert Pix image so plot is not all black.

--- R/plotPix.R
+++ R/plotPix.R
@@ -3,10 +3,11 @@ plot.Pix =
     # p = pixRead("inst/images/DifferentFonts.png")
     # p2 = pixConvertTo8(p)
     #
-function(x, y, rgb = pixGetDepth(x) > 8, pixMax = NA, ...)   
+function(x, y, rgb = pixGetDepth(x) > 8, pixMax = NA, invert = FALSE, ...)   
 {
     pix = as.raster.Pix(x, rgb, pixMax)
-
+    if(invert)
+        pix = (pix - max(pix)) * -1
 #    opar = par(no.readonly = TRUE)
 #    on.exit(par(opar))
 #    par(mar = c(2.1, 2.1, 4.1, 2.1))
