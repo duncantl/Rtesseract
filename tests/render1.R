800c3ee5a21aaf48c3311898757433779148407d
Duncan Temple Lang <duncan@r-project.org>
2018-11-21 17:46:31 -0800

    toPDF() doesn't necessarily work with tesseract 4.0

--- tests/render1.R
+++ tests/render1.R
@@ -6,7 +6,7 @@ out = Rtesseract:::removeExtension(basename(f))
 a = toHTML(f, outFile = out)
 b = toOSD(f, out)
 c = toTSV(f, outFile = out)
-d = toPDF(f, outFile = out)
+d = try(toPDF(f, outFile = out))
 e = toBoxText(f, out)
 # Not exporting UNLV
 #f = toUNLV(f, out)
