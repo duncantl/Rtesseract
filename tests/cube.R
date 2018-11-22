d28a54bdae294e61ac07c3c78c69335bd332c87c
Duncan Temple Lang <duncan@r-project.org>
2018-11-21 16:54:46 -0800

    only run these conditionally

--- tests/cube.R
+++ tests/cube.R
@@ -3,6 +3,7 @@ library(Rtesseract)
 
 f = system.file("trainingSample", "eng.tables.exp0.png", package = "Rtesseract")
 
+if(exists('OEM_TESSERACT_CUBE_COMBINED')) {
 ts = tesseract(init = FALSE)
 Init(ts, engineMode = 'OEM_TESSERACT_CUBE_COMBINED')
 SetPageSegMode(ts, 'psm_auto')
@@ -11,8 +12,9 @@ Recognize(ts)
 Rtesseract:::oem(ts)
 
 bbox = GetBoxes(ts)
+}
 
-
+if(exists('OEM_CUBE_ONLY')) {
 ts2 = tesseract(init = FALSE)
 Init(ts2, engineMode = 'OEM_CUBE_ONLY')
 SetPageSegMode(ts2, 'psm_auto')
@@ -20,4 +22,4 @@ SetInputName(ts2, f)
 Recognize(ts2)
 
 bbox2 = GetBoxes(ts2)
-
+}
