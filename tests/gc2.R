994daf7a713cf2f9ede58ff6fbdb6af5807c5094
Duncan Temple Lang <duncan@r-project.org>
2018-12-27 10:37:54 -0800

    working

--- tests/gc2.R
+++ tests/gc2.R
@@ -1,13 +1,25 @@
 library(Rtesseract)
 
-
+# Read a Pix and discard it.
+f = system.file("images", "SMITHBURN_1952_p3.png", package = "Rtesseract")
 p = pixRead(f)
 rm(p)
 gc()
 
 
-
+# Read the Pix, set it as the image for a tesseract, discard it and
+# check that tesseract still has access to it. (It is a copy that tesseract created.)
 p = pixRead(f)
 t = tesseract()
-SetImage(p)
+SetImage(t, p)
+rm(p)
+gc()
+# Use the image
+b = GetText(t)
+rm(t)
+gc()
+# Clear tesseract and GC.
+
+
+
 
