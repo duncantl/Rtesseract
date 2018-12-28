b69f92d3b8bceb8c7b9e7135299971560cef3e4d
Duncan Temple Lang <duncan@r-project.org>
2018-12-27 18:28:32 -0800

    if fail to open png() device and write the file, then exit

--- tests/pixZero.R
+++ tests/pixZero.R
@@ -2,10 +2,11 @@
 library(Rtesseract)
 
 f = tempfile()
+try({
 png(f, 300, 300); plot(0, type ='n', axes = FALSE, xlab = "", ylab = ""); dev.off()
 pix = pixRead(f)
 r1 = range(pixGetPixels(pix))
 pixZero(pix)
-
+})
 
 
