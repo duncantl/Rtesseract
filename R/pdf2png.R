3f9fbb11bf836040619872e076583e9f89caed05
Duncan Temple Lang <duncan@r-project.org>
2019-08-03 15:50:44 -0700

    code to convert a pdf file to separate pages of png images using ImageMagick's convert via our pdf2png shell script.

---
+++ R/pdf2png.R
@@ -0,0 +1,70 @@
+
+
+# We may want to have the pdf2png function 
+# + create a new temporary directory
+# + copy the original file to the new temp directory, 
+# + perform the conversion there so that no other process will modify the directory
+# + move the generated files back to the original directory
+# + remove the temporary directory
+# This will guarantee that the files we find at the end will be associated with 
+# this conversion and not some other application writing into that directory at
+# the same time.
+
+
+pdf2png =
+function(file, ...,
+         convertCmd = system.file("bin/pdf2png", package = "Rtesseract"),
+         .args = list(...), .dir = tempdir(check = TRUE))
+{
+    file = normalizePath(file)
+    
+    # XXX check if files with the suffix already exist and return those if they do.
+    # Put in separate function
+    ex = getConvertedFilenames(file)
+    if(length(ex))
+       return(structure(ex, class = "Filenames"))
+
+    
+    # XXX handle ... - make specific to pdf2png (density and base) or generic as ...
+    # if .args has names, we should use those - but how
+    #  -name value or --name=value
+    
+    cur = getwd()
+    on.exit({setwd(cur); unlink(list.files(.dir, full.names = TRUE));unlink(.dir)})
+
+    setwd(.dir)
+    if(!file.copy(file, file.path(.dir, basename(file))))
+        stop("Problem copying original file")
+    
+    before = list.files(".", full = TRUE)
+    system(sprintf("%s %s %s", convertCmd, paste(.args, collapse = " "), basename(file)))
+    after = list.files(".", full = TRUE)
+    files = sort(setdiff(after, before))
+    file.copy(files, cur)
+    
+    structure(file.path(cur, files), class = "Filenames")
+}
+
+getConvertedFilenames =
+function(file, dir = dirname(file), base = rmExt(basename(file), ext),
+         suffix = "_p[0-9]+\\.png",
+         ext = getExt(basename(file)))
+    # Originally, I thought we needed the extension of the original file to put it back
+    # onto the numbered files. But of course these are png, not pdf files.
+    # So we don't need ext and so we should just leave base = rmExt(basename(file))
+    # and omit ext = 
+{
+  list.files(dir, pattern = sprintf("%s%s", base, suffix), full.names = TRUE)
+}
+
+getExt =
+function(filename)
+{
+    gsub(".*\\.", "", filename)
+}
+
+rmExt =
+function(filename, ext)
+{
+   gsub("\\.$", "", gsub(ext, "", filename, fixed = TRUE))
+}
