d178dac9b8675261bd25e48f0afc5f49dd07ec46
Duncan Temple Lang <duncan@r-project.org>
2022-03-08 12:35:19 -0800

    readImage function. How does this relate to guess file by extension in ext.R

--- R/imageFuns.R
+++ R/imageFuns.R
@@ -1,3 +1,5 @@
+if(FALSE)
+    # Old version that just uses the extension.
 readImage =
 function(filename)
 {
@@ -6,6 +8,21 @@ function(filename)
     if(grepl("\\.je?pg$", filename))
         img = jpeg::readJPEG(filename)
     if(grepl("\\.tiff?$", filename))
+        img = tiff::readTIFF(filename)
+    img
+}
+
+readImage =
+    # Version that reads the format from the header of the file.
+function(filename, format = names(readImageInfo(filename)$format))
+{
+    format = tolower(format)
+    
+    if(grepl("png", format))
+        img = png::readPNG(filename)
+    if(grepl("jpeg|jp2", format))
+        img = jpeg::readJPEG(filename)
+    if(grepl("tiff", format))
         img = tiff::readTIFF(filename)
     img
 }
