02466d9c00b3e1366ce1df7c2f87f9baee111276
Duncan Temple Lang <duncan@r-project.org>
2022-03-08 12:30:40 -0800

    apparently using a different image

--- tests/pixRGBPixels.R
+++ tests/pixRGBPixels.R
@@ -1,5 +1,6 @@
 library(Rtesseract)
-f = system.file("images", "DifferentFonts.png", package = "Rtesseract")
+# f = system.file("images", "DifferentFonts.png", package = "Rtesseract")
+f = system.file("images", "OCRSample2.png", package = "Rtesseract")
 p = pixRead(f)
 z = pixGetRGBPixels(p)
 class(z)
